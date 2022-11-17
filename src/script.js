1500b629cf32e44f6938af7c1e284ec355d44911
Hfn <55591161+titocs@users.noreply.github.com>
2022-11-18 00:41:10 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -57,6 +57,8 @@ startButton.addEventListener('click', () => {
         }
         if(remainingTime >= 1000 && remainingTime <= 2000){
             startButton.disabled = false;
+            sound.pause();
+            sound.currentTime = 0;
         }
     }
     

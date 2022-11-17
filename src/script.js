c181831fbd7e357f931837227d504bd72b28af3f
Hfn <55591161+titocs@users.noreply.github.com>
2022-11-17 16:39:29 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -52,7 +52,7 @@ startButton.addEventListener('click', () => {
                 <div id="seconds" class="text-[30vw] font-semibold flex-grow" style="flex-grow: 1;">00</div>
             `;
         }
-        if(remainingTime >= 0 && remainingTime <= 6700){
+        if(remainingTime >= 0 && remainingTime <= 7000){
             beepSound.play();
         }
         if(remainingTime >= 1000 && remainingTime <= 2000){
